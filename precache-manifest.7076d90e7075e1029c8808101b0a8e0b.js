44076b5833465f3e1204d16f97a9155a01254752
Luu Tu <luutu@MacBook-Pro-cua-Luu.local>
2020-03-25 14:36:06 +0700

    Updates

---
+++ precache-manifest.7076d90e7075e1029c8808101b0a8e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50573146ede8425db386c76cf8fef9e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "afcc057fb978b18cfce1",
+    "url": "/monster-rolodex/static/css/main.6f766123.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcc057fb978b18cfce1",
+    "url": "/monster-rolodex/static/js/main.6faeaf44.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
